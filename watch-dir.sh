13fb3c02c6f29ce8dc0fd1c539450310a6473c15
Cezary Butler <cezary.butler@gmail.com>
2017-10-08 21:20:36 +0200

    Paths moved to environment variables, file uplaod moved to function

--- watch-dir.sh
+++ watch-dir.sh
@@ -6,10 +6,15 @@ DIRECTORY=$1
 
 echo 'Starting directory watch on ' $DIRECTORY
 
-rm -f /home/gsync/pipes/files
-rm -f /home/gsync/pipes/split
-mkfifo /home/gsync/pipes/files
-mkfifo /home/gsync/pipes/split
+PWD=$(pwd)
+GSYNCD=${PWD}/.gsync
+
+mkdir -p ${GSYNCD}/pipes
+FILES=${GSYNCD}/pipes/files
+SPLIT=${GSYNCD}/pipes/split
+rm -f ${FILES} ${SPLIT}
+mkfifo ${FILES}
+mkfifo ${SPLIT}
 
 INOTIFY_ARGS="--format '%w%f' -rme close_write"
 
@@ -19,16 +24,28 @@ if [ $2 ]
 fi
 
 #todo sposob na monitorowanie tylko najnowszych w strukturze po 1 na poziom na pewno bez -r
-inotifywait $INOTIFY_ARGS $DIRECTORY  > /home/gsync/pipes/files &
+inotifywait $INOTIFY_ARGS $DIRECTORY  > ${FILES} &
 
 echo 'Initialized inotify'
 sleep 1
 
-cat /home/gsync/pipes/files | tee /home/gsync/pipes/split  &
+cat ${FILES} | tee ${SPLIT}  &
 
 echo 'Stream redirected for split'
 
-split -ul 25 --filter='tar --no-seek -cjf - -T - | gdrive upload - --no-progress -p 0B3lkE4i92Vu6bTEzeGVkd19qRDQ "ZM$(date -Iseconds).tar.bz2"' /home/gsync/pipes/split &
+function tar-live {
+  tar --no-seek -cjf - -T -
+}
+
+GDIVE_TARGET='0B3lkE4i92Vu6bTEzeGVkd19qRDQ'
+
+function gdrive-upload {
+  gdrive upload - --no-progress -p ${GDIVE_TARGET} "ZM$(date -Iseconds).tar.bz2"
+}
+
+#split -ul 25 --filter='tar-live | gdrive-upload' ${SPLIT} &
+
+split -ul 2 --filter'stat' &
 
 echo 'Split initialized'
 sleep 1
@@ -39,5 +56,4 @@ wait
 
 echo 'All done!'
 
-rm -f /home/gsync/pipes/files
-rm -f /home/gsync/pipes/split
+rm -f ${FILES} ${SPLIT}
